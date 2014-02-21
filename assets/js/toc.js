635eaa35f25673aab771084408f47fac671f8214
Kaeden <snufless@yahoo.com>
2014-02-21 19:59:46 +0200

    Updating TOC generator

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,5 +1,7 @@
 // https://github.com/ghiculescu/jekyll-table-of-contents
 $(document).ready(function() {
+  var no_back_to_top_links = false
+
   var headers = $('h1, h2, h3, h4, h5, h6').filter(function() {return this.id}), // get all headers with an ID
       output = $('.toc');
   if (!headers.length || headers.length < 3 || !output.length)
@@ -9,12 +11,12 @@ $(document).ready(function() {
   var highest_level = headers.map(function(_, ele) { return get_level(ele) }).get().sort()[0]
   var return_to_top = '<i class="icon-arrow-up back-to-top"> </i>'
 
-  var level = get_level(headers[0]), this_level, html = "<h3>Contents</h3><ol>";
+  var level = get_level(headers[0]), this_level, html = "<i>Jump to...</i> <ol>";
   headers.on('click', function() {
-    window.location.hash = this.id
+    if (!no_back_to_top_links) window.location.hash = this.id
   }).addClass('clickable-header').each(function(_, header) {
     this_level = get_level(header);
-    if (this_level === highest_level) {
+    if (!no_back_to_top_links && this_level === highest_level) {
       $(header).addClass('top-level-header').after(return_to_top)
     }
     if (this_level === level) // same level as before; same indenting
@@ -26,9 +28,11 @@ $(document).ready(function() {
     level = this_level; // update for the next one
   });
   html += "</ol>";
-  $(document).on('click', '.back-to-top', function() {
-    $(window).scrollTop(0)
-    window.location.hash = ''
-  })
+  if (!no_back_to_top_links) {
+    $(document).on('click', '.back-to-top', function() {
+      $(window).scrollTop(0)
+      window.location.hash = ''
+    })
+  }
   output.hide().html(html).show('slow');
 });
